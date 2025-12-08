365d507c84a063087ca6b39ee5ccd09d86e41e60
Khan Trần <kcoder282@gmail.com>
2025-12-08 16:55:31 +0700

    push

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -29,78 +29,6 @@ const API = {
   getList: () => axios.get('/target').then((res) => res.data as TargetDatabase[]),
   asyncTarget: (target: TargetDatabase) => axios.post(`/target`, target).then((res) => res.data.id as number),
   getTarget: (id: number) => axios.get(`/target/${id}`).then((res) => res.data as TargetDatabase),
-  // chat: async (msg: Message, onMessage: React.Dispatch<React.SetStateAction<Message>>, isCallFn?: boolean) => {
-  //   const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/ai-chat`, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Authorization: `Bearer ${localStorage.getItem('access_token')}`
-  //     },
-  //     body: JSON.stringify({ messages: msg.filter(x => !x.noServer).slice(-10) }),
-  //   });
-
-  //   const reader = res.body?.getReader();
-  //   const decoder = new TextDecoder();
-  //   if (!reader) return;
-  //   const messageResponse: Message[number] = { role: 'assistant', content: '' };
-  //   if (!isCallFn) {
-  //     onMessage([...msg, messageResponse]);
-  //   } else {
-  //     onMessage((pre) => [...pre, messageResponse]);
-  //   }
-
-  //   while (true) {
-  //     const { value, done } = await reader.read();
-  //     if (done) break;
-  //     const msgData = decoder.decode(value);
-  //     messageResponse.content += msgData;
-  //     onMessage((pre) => [...pre])
-  //   }
-
-  //   if (messageResponse.content.startsWith("[") && messageResponse.content.endsWith("]")) {
-
-  //     const functionData = JSON.parse(messageResponse.content)
-  //     if (functionData[0] === 'evaluate_target') {
-  //       onMessage((pre) => pre.slice(0, -1));
-  //       await API.chat([{ role: 'user', content: `[nofunction] Tôi có dữ liệu này ${JSON.stringify(await getScore())}, chỉ dựa vào dữ liệu này hãy đáng giá mục tiêu các môn học giúp tôi, môn nào chưa nhập điểm thì không đánh giá là chưa đạt, hãy giúp tôi đưa ra lời khuyên để đạt được mục tiêu các môn học.` }], onMessage, true);
-  //     }
-  //     if (functionData[0] === 'add_score') {
-  //       try {
-  //         const now = new Date();
-  //         const target = await db.targets.where('subject').equals(functionData[1]['subject']).first();
-  //         if (target) {
-  //           const score = await db.scores.where({ subject: functionData[1]['subject'] as Subjects, exams: functionData[1]['exams'] as Exams }).count();
-  //           if (target.exams[functionData[1]['exams'] as Exams] >= score) {
-  //             messageResponse.content = `Đã nhập đầy đủ điểm nên không thể thêm điểm nữa`
-  //             messageResponse.noServer = true;
-  //             onMessage((pre) => [...pre])
-  //             return
-  //           }
-  //         } else {
-  //           messageResponse.content = `Chưa đặt mục tiêu cho môn học ${functionData[1]['subject']} nên không thể thêm điểm`
-  //           messageResponse.noServer = true;
-  //           onMessage((pre) => [...pre])
-  //           return
-  //         }
-  //         await db.scores.add({
-  //           subject: functionData[1]['subject'] as Subjects,
-  //           exams: functionData[1]['exams'] as Exams,
-  //           score: functionData[1]['score'] as number,
-  //           createdAt: now.toISOString(),
-  //           updatedAt: now.toISOString(),
-  //         });
-  //         messageResponse.content = `Đã ghi nhận bạn được ${functionData[1]['score']} điểm (Môn ${functionData[1]['subject']}, ${functionData[1]['exams']})`;
-  //         messageResponse.noServer = true;
-  //         onMessage((pre) => [...pre])
-  //       } catch {
-  //         messageResponse.content = `Đã xảy ra lỗi khi gọi hàm...`;
-  //         messageResponse.noServer = true;
-  //         onMessage((pre) => [...pre])
-  //       }
-  //     }
-  //   }
-
-  // }
   chat: async (target: TargetData, onMessage: (msg: string) => void) => {
     const scores = await db.scores.toCollection().filter(x => x.subject === target.subject).toArray()
     const avg = [
@@ -136,7 +64,7 @@ const API = {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem('access_token')}`
       },
-      body: JSON.stringify({ messages: `Dựa trên Object ${JSON.stringify(objectPromt)} hãy đánh giá môn học, tôi có thể đạt được mục tiêu không? - làm sao để đạt được mục tiêu, cho tôi lời khuyên` }),
+      body: JSON.stringify({ message: `Dựa trên Object ${JSON.stringify(objectPromt)} hãy đánh giá môn học, tôi có thể đạt được mục tiêu không? - làm sao để đạt được mục tiêu, cho tôi lời khuyên` }),
     });
     let msg = "";
     const reader = res.body?.getReader();
